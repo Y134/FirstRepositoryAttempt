2755dbf58ffd4c2b0b9fbc5e7c78b77c95ad54cf
Y134 <mincentvilo@gmail.com>
2017-09-02 05:17:45 -0700

    Add files via upload

---
+++ JSStalker.js
@@ -0,0 +1,8 @@
+var firstName = prompt("What is your first name?");
+var lastName = prompt("What is your last name?");
+var userAge = prompt("What is your age?");
+alert("Please take look at your console.");
+var sentenceName = "Your full name is " + firstName  + " " + lastName + ".";
+console.log(sentenceName);
+var sentenceAge = "You are " + userAge + " years old.";
+console.log(sentenceAge);
